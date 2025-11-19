69578705135b3d3c0da80c8434ce5b12b11b0e3c
haha_mj <haha_mj@naver.com1~>
2025-11-19 16:27:16 +0900

    add

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-new fullpage("#fullpage", {
-  // 옵션 설정
-  // sectionsColor: ["#f2f2f2", "#4BBFC3", "#7BAABE", "whitesmoke"],
-  anchors: ["firstPage", "secondPage", "thirdPage", "fourthPage"],
-  menu: "#menu",
-  scrollingSpeed: 1000,
+$(document).ready(function () {
+  $("#fullpage").fullpage({
+    autoScrolling: true,
+    scrollHorizontally: true,
+    scrollingSpeed: 700,
+  });
 });
